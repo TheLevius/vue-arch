1fca1c493c0a2b40a45bc83eecfac02170cf0e48
TheLevius <reflevius@gmail.com>
2024-07-20 12:38:01 +0300

    feat: add fetching data from mock api

--- src/App.vue
+++ src/App.vue
@@ -2,57 +2,48 @@
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
-
-const items = [
-  {
-    id: 1,
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 12999,
-    imageUrl: '/sneakers/sneakers-1.jpg'
-  },
-  {
-    id: 2,
-    title: 'Мужские Кроссовки Nike Air Max 270',
-    price: 15600,
-    imageUrl: '/sneakers/sneakers-2.jpg'
-  },
-  {
-    id: 3,
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 8499,
-    imageUrl: '/sneakers/sneakers-3.jpg'
-  },
-  {
-    id: 4,
-    title: 'Кроссовки Puma X Aka Boku Future Rider',
-    price: 7800,
-    imageUrl: '/sneakers/sneakers-4.jpg'
-  },
-  {
-    id: 5,
-    title: 'Кроссовки Future Rider',
-    price: 9550,
-    imageUrl: '/sneakers/sneakers-5.jpg'
-  },
-  {
-    id: 6,
-    title: 'Кроссовки Black Edition',
-    price: 16999,
-    imageUrl: '/sneakers/sneakers-6.jpg'
-  },
-  {
-    id: 7,
-    title: 'Кроссовки Orange Boomb Edition',
-    price: 7499,
-    imageUrl: '/sneakers/sneakers-7.jpg'
-  },
-  {
-    id: 8,
-    title: 'Кроссовки Nike Air Max 270',
-    price: 15600,
-    imageUrl: '/sneakers/sneakers-8.jpg'
+import axios from 'axios'
+import { onMounted, reactive, ref, watch } from 'vue'
+const items = ref([])
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+const pickQueries = (queries) => {
+  const dictQuery = {
+    sortBy: 'sortBy',
+    searchQuery: 'title'
   }
-]
+  return queries.reduce((queryObj, el) => {
+    if (filters[el]) {
+      const currentKey = dictQuery[el]
+      if (currentKey === 'title') {
+        queryObj[currentKey] = `*${filters[el]}*`
+      } else {
+        queryObj[currentKey] = filters[el]
+      }
+    }
+    return queryObj
+  }, {})
+}
+
+const fetchItems = async () => {
+  const params = pickQueries(Object.keys(filters))
+  const { data } = await axios
+    .get('https://604781a0efa572c1.mokky.dev/items', {
+      params
+    })
+    .catch(console.error)
+  items.value = data
+}
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 <template>
   <!-- <Drawer /> -->
@@ -62,16 +53,22 @@ const items = [
       <div class="flex justify-between items-center mb-8">
         <h2 class="text-3xl font-bold">All Sneakers</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none" name="" id="">
-            <option value="by name">by name</option>
-            <option value="asc by price">ascending price</option>
-            <option value="desc by price">descending price</option>
+          <select
+            @change="onChangeSelect"
+            class="py-2 px-3 border rounded-md outline-none"
+            name=""
+            id=""
+          >
+            <option value="name">by name</option>
+            <option value="price">ascending price</option>
+            <option value="-price">descending price</option>
           </select>
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
             <input
               class="border rounded-md py-2 pl-11 pr-4 outline-none"
               type="text"
+              @change="onChangeSearchInput"
               placeholder="Search..."
             />
           </div>
